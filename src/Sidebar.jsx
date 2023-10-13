7c087870582ecf4302e2de0c28e19f63fb806e03
haseebzahid1 <mmianhaseeb444@gmail.com>
2023-10-13 17:54:02 +0500

    Build Category Component

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -2,15 +2,16 @@ import './Sidebar.css'
 import Category from './Sidebar/Category/Category'
 import Price from './Sidebar//Price/Price'
 import Colors from './Sidebar/Color/Colors'
-const Sidebar = () => {
+
+const Sidebar = ({handleChange}) => {
   return (
     <section className="sidebar">
     <div className="logo-container">
       <h1>🛒</h1>
     </div>
-    <Category  />
+    <Category handleChange={() => console.log('Category')} />
     <Price  />
-    <Colors  />
+    <Colors  handleChange={() => console.log('Colors')} />
   </section>
   )
 }
