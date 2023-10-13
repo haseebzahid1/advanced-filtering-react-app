43feb213014db03ccb71f1a21b2114cdaf534f0e
haseebzahid1 <mmianhaseeb444@gmail.com>
2023-10-13 23:17:36 +0500

    fake data api pass product

--- src/App.js
+++ src/App.js
@@ -1,22 +1,83 @@
-import React from 'react'
-import Nav from './navigation/Nav'
-import Products from './Product/Products'
-import Recommended from './Recommended/Recommended'
-
+import React, { useState } from "react";
+import Nav from "./navigation/Nav";
+import Products from "./Product/Products";
+import Recommended from "./Recommended/Recommended";
 import "./index.css";
-import Sidebar from './Sidebar';
-
-
+import Sidebar from "./Sidebar";
+import ProductData from "./db/data";
+import Card from './component/Card'
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  // ----------- Input Filter -----------
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const filteredItems = ProductData.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+
+  // ----------- Radio Filtering -----------
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  // ------------ Button Filtering -----------
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  function filteredData(ProductData, selected, query) {   // function start
+
+    let filteredProducts = ProductData;  // All product data
+
+    // Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    // Applying selected filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+
+
+  }   // filteredData (function end) 
+
+  const result = filteredData(ProductData, selectedCategory, query);
+
   return (
     <>
-    <Sidebar  />
-    <Nav />
-    <Recommended />
-    <Products />
-    
+      <Sidebar />
+      <Nav />
+      <Recommended />
+      <Products result={result} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Product/Products.jsx
+++ src/Product/Products.jsx
@@ -1,35 +1,8 @@
 import './Products.css'
-import Card from '../component/Card';
-import { AiFillStar } from "react-icons/ai";
-const Products = () => {
+
+const Products = ({result}) => {
   return (
-  <div className='card-container'>    
-      <Card 
-    img='https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg'
-    title= "Nike Air Monarch IV"
-    star = {<AiFillStar className="rating-star" />}
-    reviews= "123 reviews"
-    prevPrice= "$140,00"
-    newPrice= "200"
-    />
-      <Card 
-    img='https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg'
-    title= "Nike Air Monarch IV"
-    star = {<AiFillStar className="rating-star" />}
-    reviews= "123 reviews"
-    prevPrice= "$140,00"
-    newPrice= "200"
-    />
-      <Card 
-    img='https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg'
-    title= "Nike Air Monarch IV"
-    star = {<AiFillStar className="rating-star" />}
-    reviews= "123 reviews"
-    prevPrice= "$140,00"
-    newPrice= "200"
-    />
- 
-  </div>  
+    <section className="card-container">{result}</section>
   )
 }
 
