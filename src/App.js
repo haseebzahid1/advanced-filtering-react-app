44b054286725f6fb5d9a6f3f1e1b83cd9053e746
haseebzahid1 <mmianhaseeb444@gmail.com>
2023-10-13 16:54:11 +0500

    Build Card Component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react'
 import Nav from './navigation/Nav'
 import Products from './Product/Products'
 import Recommended from './Recommended/Recommended'
+
 import "./index.css";
+
+
 const App = () => {
   return (
     <>
     <Nav />
     <Products />
     <Recommended />
+    
     </>
   )
 }
--- src/Product/Products.jsx
+++ src/Product/Products.jsx
@@ -1,10 +1,35 @@
 import './Products.css'
-
+import Card from '../component/Card';
+import { AiFillStar } from "react-icons/ai";
 const Products = () => {
   return (
-  <div>
-      <div>Products</div>
-  </div>
+  <div className='card-container'>    
+      <Card 
+    img='https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg'
+    title= "Nike Air Monarch IV"
+    star = {<AiFillStar className="rating-star" />}
+    reviews= "123 reviews"
+    prevPrice= "$140,00"
+    newPrice= "200"
+    />
+      <Card 
+    img='https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg'
+    title= "Nike Air Monarch IV"
+    star = {<AiFillStar className="rating-star" />}
+    reviews= "123 reviews"
+    prevPrice= "$140,00"
+    newPrice= "200"
+    />
+      <Card 
+    img='https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg'
+    title= "Nike Air Monarch IV"
+    star = {<AiFillStar className="rating-star" />}
+    reviews= "123 reviews"
+    prevPrice= "$140,00"
+    newPrice= "200"
+    />
+ 
+  </div>  
   )
 }
 
