74d72d36aeb76c07749335cc031ed92f9d033a16
haseebzahid1 <mmianhaseeb444@gmail.com>
2023-10-14 11:43:22 +0500

    Category Component Category

--- src/Sidebar/Category/Category.jsx
+++ src/Sidebar/Category/Category.jsx
@@ -3,6 +3,13 @@ import  Input from '../../component/Input'
 
 const Category = ({handleChange}) => {
 
+  const inputCategory = [
+    { title: 'Sneakers', value: "sneakers", name: "test" },
+    { title: "Flats", value: "flats", name: "test" },
+    { title: "Sandals", value: "sandals", name: "test" },
+    { title: "Heels", value: "heels", name: "test" },
+  ];
+
     return (
       <div>
         <h2 className="sidebar-title">Category</h2>
@@ -12,30 +19,17 @@ const Category = ({handleChange}) => {
             <input onChange={handleChange} type="radio" value="" name="test" />
             <span className="checkmark"></span>All
           </label>
+
+          {inputCategory.map((input, index) => (
           <Input
+            key={index} // It's important to provide a unique key for React components rendered in a loop
             handleChange={handleChange}
-            value="sneakers"
-            title="Sneakers"
-            name="test"
+            value={input.value}
+            title={input.title}
+            name={input.name}
           />
-          <Input
-            handleChange={handleChange}
-            value="flats"
-            title="Flats"
-            name="test"
-          />
-          <Input
-            handleChange={handleChange}
-            value="sandals"
-            title="Sandals"
-            name="test"
-          />
-          <Input
-            handleChange={handleChange}
-            value="heels"
-            title="Heels"
-            name="test"
-          />
+        ))}
+          
         </div>
       </div>
     );
