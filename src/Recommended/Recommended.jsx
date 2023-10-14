0c9338ac5bc6bcfb2baea200c77f199e51275762
haseebzahid1 <mmianhaseeb444@gmail.com>
2023-10-14 12:34:27 +0500

    Recommended Component rendering

--- src/Recommended/Recommended.jsx
+++ src/Recommended/Recommended.jsx
@@ -1,22 +1,31 @@
-import './Recommended.css'
-import Button from '../component/Button';
-
+import "./Recommended.css";
+import Button from "../component/Button";
 
 const Recommended = ({ handleClick }) => {
-    return (
-      <>
-        <div>
-          <h2 className="recommended-title">Recommended</h2>
-          <div className="recommended-flex">
-            <Button onClickHandler={handleClick} value="" title="All Products" />
-            <Button onClickHandler={handleClick} value="Nike" title="Nike" />
-            <Button onClickHandler={handleClick} value="Adidas" title="Adidas" />
-            <Button onClickHandler={handleClick} value="Puma" title="Puma" />
-            <Button onClickHandler={handleClick} value="Vans" title="Vans" />
-          </div>
+  const buttonData = [
+    { value: "", title: "All Products" },
+    { value: "Nike", title: "Nike" },
+    { value: "Adidas", title: "Adidas" },
+    { value: "Puma", title: "Puma" },
+    { value: "Vans", title: "Vans" },
+  ];
+  return (
+    <>
+      <div>
+        <h2 className="recommended-title">Recommended</h2>
+        <div className="recommended-flex">
+          {buttonData.map((button, index) => (
+            <Button
+              key={index} // It's important to provide a unique key for React components rendered in a loop
+              onClickHandler={handleClick}
+              value={button.value}
+              title={button.title}
+            />
+          ))}
         </div>
-      </>
-    );
-  };
-  
-  export default Recommended;
+      </div>
+    </>
+  );
+};
+
+export default Recommended;
